45df60504bce6f6623ba13caf3e265c9d7a6a1e5
ksawerykowalczuk <ksawery.kowalczuk@gmail.com>
2024-07-30 19:07:50 +0200

    -koniec? -NIE >:(

--- memoryTest.cpp
+++ memoryTest.cpp
@@ -2,8 +2,33 @@
 
 using namespace std;
 
-int main ()
+void dodajelement(int *tablica, int dlugosc, int wartosc)
 {
-    cout << "heloł worl";
-    
+    dlugosc++;
+    (int*) realloc(tablica, dlugosc*sizeof(int));
+    tablica[dlugosc-1]= wartosc;
+}
+
+int main()
+{
+    int dlugosc = 0;
+    cout << "podaj dlugosc tablicy: ";
+    cin >> dlugosc;
+    int *tablica;
+    tablica = (int *)malloc(dlugosc * sizeof(int));
+    for (int i = 0; i < dlugosc; i++)
+    {
+        tablica[i] = 5;
+    }
+    int wartosc;
+    cout << "podajwartosckolejnegoelementu :) ------> ";
+    cin >> wartosc;
+    dodajelement(tablica, dlugosc, wartosc);
+    dlugosc++;
+    for (int i = 0; i < dlugosc; i++)
+    {
+        cout << tablica[i];
+    }
+    cout << "koniec";
+    free(tablica);
 }
